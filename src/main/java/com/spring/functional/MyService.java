4dc2308a12275fe58c2c1fa48964a8ea1e78cd18
Amen Allah BHAR <amenallah.bhar.ext@boursorama.fr>
2018-03-20 16:25:08 +0100

    Initial commit

---
+++ src/main/java/com/spring/functional/MyService.java
@@ -0,0 +1,12 @@
+package com.spring.functional;
+
+
+import java.util.Random;
+
+public class MyService {
+
+    public int getRandomNumber() {
+        return (new Random().nextInt(10));
+    }
+
+}
